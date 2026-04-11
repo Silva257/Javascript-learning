6317c588e38401e05cb71e4411021567b063e60b
Silva Charles <silvacharlesniyokindi@gmail.com>
2026-04-11 21:20:05 +0300

    new amazon project building

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -4,7 +4,7 @@ const products = [
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
-      stars: 4.5,
+      stars: 'images/ratings/rating-35.png',
       count: 87
     },
     priceCents: 1090,
